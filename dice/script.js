9bea8b5c8a5b16c85aab4f99c971389263ff0966
Tetiana Chykalova <chikalova.t@gmail.com>
2022-12-21 14:18:00 +0100

    roll the dice

--- dice/script.js
+++ dice/script.js
@@ -11,11 +11,15 @@ const btnLeavePoints = document.querySelector('#leave-points');
 
 const dice = document.querySelector('#dice');
 
+const gamer0 = document.querySelector('#gamer-0')
 const gamer0generalPoints = document.querySelector('#general-points-0'); //DOM ELEMENT
 const gamer0turnPoints = document.querySelector('#turn-points-0'); //DOM ELEMENT
 
+const gamer1 = document.querySelector('#gamer-1')
 const gamer1generalPoints = document.querySelector('#general-points-1'); //DOM ELEMENT
 const gamer1turnPoints = document.querySelector('#turn-points-1'); //DOM ELEMENT
+
+let currentPoints = 0
 
 
 //Start initial
@@ -24,3 +28,22 @@ gamer0turnPoints.textContent = '0'
 
 gamer1generalPoints.textContent = '0'
 gamer1turnPoints.textContent = '0'
+
+dice.classList.add('hide');
+
+btnThrowDice.addEventListener('click', function () {
+    const diceNumber = Math.trunc(Math.random() * 6) + 1
+
+    dice.classList.remove('hide')
+//     src через switch
+    dice.src = `img/dice${diceNumber}.png`
+
+    if(diceNumber !== 1) {
+        currentPoints += diceNumber
+        gamer0.classList.contains('gamer-active') ?
+            gamer0turnPoints.textContent = currentPoints : gamer0turnPoints.textContent = currentPoints
+    }
+    else {
+
+    }
+})
